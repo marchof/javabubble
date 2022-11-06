2eb51ddbb0651a86cd7efbc65ee808518b20f1ab
Marc R. Hoffmann <hoffmann@mountainminds.com>
2022-11-06 13:04:39 +0100

    Add GitHub handles
    Also make all handles optional.

--- generator/src/main/java/org/javabubble/generator/model/JavaPerson.java
+++ generator/src/main/java/org/javabubble/generator/model/JavaPerson.java
@@ -1,5 +1,7 @@
 package org.javabubble.generator.model;
 
+import java.util.Optional;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -15,4 +17,22 @@ public record JavaPerson(
 		@JsonInclude(Include.NON_NULL) String github
 
 ) {
+
+	public Optional<String> twitterLink() {
+		return Optional.ofNullable(twitter()) //
+				.map(handle -> handle.substring(1)) //
+				.map("https://twitter.com/%s"::formatted);
+	}
+
+	public Optional<String> fediverseLink() {
+		return Optional.ofNullable(fediverse()) //
+				.map(handle -> handle.split("@")) //
+				.map("https://%3$s/@%2$s"::formatted);
+	}
+
+	public Optional<String> githubLink() {
+		return Optional.ofNullable(github()) //
+				.map("https://github.com/%s/"::formatted);
+	}
+
 }
