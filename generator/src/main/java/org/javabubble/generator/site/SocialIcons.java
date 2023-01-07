bd914f03093748e566ff6fb7bdbfcf57030d7f64
Marc R. Hoffmann <hoffmann@mountainminds.com>
2023-01-07 20:44:24 +0100

    Render table more compact for better extensibility and mobile support (#207)
    1) Use icons for social platforms
    2) Combine identical local handles

---
+++ generator/src/main/java/org/javabubble/generator/site/SocialIcons.java
@@ -0,0 +1,33 @@
+package org.javabubble.generator.site;
+
+import static j2html.TagCreator.rawHtml;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+
+import j2html.tags.DomContent;
+
+enum SocialIcons {
+
+	mastodon, twitter, github, reddit;
+
+	DomContent svg() {
+		return rawHtml("<svg class=\"socialicon\"><use xlink:href=\"#%s\"/></svg>".formatted(name()));
+	}
+
+	static DomContent symbols() {
+		var buffer = new StringWriter();
+		buffer.write("<svg display=\"none\">");
+		for (var i : values()) {
+			try (var in = SocialIcons.class.getResourceAsStream("assets/%s.svg".formatted(i.name()))) {
+				buffer.write(new String(in.readAllBytes(), StandardCharsets.UTF_8));
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		buffer.write("</svg>");
+		return rawHtml(buffer.toString());
+	}
+
+}
