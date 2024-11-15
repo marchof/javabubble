3eb76cbeffc8b036e04428846db83df76b515805
Stefano Cordio <stefano.cordio@gmail.com>
2024-11-16 00:09:16 +0100

    Use word character class with Unicode support for LinkedIn validation

--- generator/src/main/java/org/javabubble/generator/model/LinkedInHandle.java
+++ generator/src/main/java/org/javabubble/generator/model/LinkedInHandle.java
@@ -2,9 +2,11 @@ package org.javabubble.generator.model;
 
 import java.util.regex.Pattern;
 
+import static java.util.regex.Pattern.UNICODE_CHARACTER_CLASS;
+
 public final class LinkedInHandle extends Handle {
 
-	private static final Pattern HANDLE_PATTERN = Pattern.compile("[A-Za-z0-9_\\-]+");
+	private static final Pattern HANDLE_PATTERN = Pattern.compile("[\\w\\-]+", UNICODE_CHARACTER_CLASS);
 
 	public LinkedInHandle(String handle) {
 		super(handle, HANDLE_PATTERN);
